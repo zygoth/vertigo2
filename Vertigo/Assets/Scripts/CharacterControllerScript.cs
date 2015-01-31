0cf2b24621baf50950d2d3084398d182cc959a88
zygoth <zygoth2@gmail.com>
2015-01-31 14:23:30 -0700

    startin work on ravity switchin

--- Vertigo/Assets/Scripts/CharacterControllerScript.cs
+++ Vertigo/Assets/Scripts/CharacterControllerScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CharacterControllerScript: MonoBehaviour
 {
 	public float maxSpeed = 10f;
+	public Vector2 gravityVector = new Vector2 (0f, -30f);
 	bool facingRight = true;
 	
 	Animator anim;
@@ -13,6 +14,8 @@ public class CharacterControllerScript: MonoBehaviour
 	float groundRadius = 0.2f;
 	public LayerMask whatIsGround;
 	public float jumpForce = 700f;
+	public enum gravityDirection {DOWN, LEFT, UP, RIGHT};
+	public gravityDirection gravity = gravityDirection.DOWN;
 	
 	void Start()
 	{
@@ -21,34 +24,65 @@ public class CharacterControllerScript: MonoBehaviour
 
 	void doGroundCheck()
 	{
-
-	}
-	
-	void FixedUpdate()
-	{
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
 		anim.SetBool ("Ground", grounded);
+	}
 
-
+	void doMovement()
+	{
 		float move = Input.GetAxis("Horizontal");
 		
 		anim.SetFloat("Speed", Mathf.Abs(move));
-		
-		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
-		
+
+		if(Mathf.Abs(move) > 0)
+		{
+			rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+		}
+
 		if(move > 0 &&!facingRight)
 			Flip();
 		else if (move < 0 && facingRight)
 			Flip();
 	}
+	
+	void FixedUpdate()
+	{
+		doGroundCheck ();
 
-	void Update()
+		doMovement ();
+
+		rigidbody2D.AddForce (gravityVector);
+	}
+
+	void doJumpCheck()
 	{
 		if (grounded && Input.GetKeyDown(KeyCode.Space))
 		{
 			anim.SetBool ("Ground", false);
 			rigidbody2D.AddForce (new Vector2(0, jumpForce));
 		}
+	}
+
+	void RotateLeft () 
+	{
+		transform.Rotate (Vector3.forward * -90);
+		gravity = (gravityDirection)((((int)gravity) + 1) % 4);
+		gravityVector = new Vector2 (gravityVector.y, -gravityVector.x);
+	}
+
+	void doGravityCheck()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			RotateLeft();
+		}
+	}
+
+	void Update()
+	{
+		doJumpCheck ();
+
+		doGravityCheck ();
 	}
 	
 	void Flip()
