79cea4ee7175679848708483bee67bd1eb6be61c
zygoth <zygoth2@gmail.com>
2015-02-06 11:23:57 -0700

    added all gravity blocks

---
+++ Vertigo/Assets/Scripts/RightGravityBlock.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RightGravityBlock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		GameObject character = GameObject.Find ("Character");
+		CharacterControllerScript script = (CharacterControllerScript) character.GetComponent ("CharacterControllerScript");
+		script.switchGravity (CharacterControllerScript.gravityDirection.RIGHT);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Vertigo/Assets/Scripts/UpGravityBlock.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpGravityBlock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		GameObject character = GameObject.Find ("Character");
+		CharacterControllerScript script = (CharacterControllerScript) character.GetComponent ("CharacterControllerScript");
+		script.switchGravity (CharacterControllerScript.gravityDirection.UP);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
