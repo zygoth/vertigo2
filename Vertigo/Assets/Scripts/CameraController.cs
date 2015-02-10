c4c7fddc2254fc702bd62ffad2149d747acc011b
zygoth <zygoth2@gmail.com>
2015-02-10 11:41:54 -0700

    screen control

---
+++ Vertigo/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	public enum ScreenMode {HORIZONTAL, VERTICAL, FREE};
+	public ScreenMode currentMode;
+
+	// Use this for initialization
+	void Start () 
+	{
+		currentMode = ScreenMode.FREE;
+	}
+
+	void FixedUpdate () 
+	{
+		GameObject character = GameObject.Find ("Character");
+		CharacterControllerScript script = (CharacterControllerScript) character.GetComponent ("CharacterControllerScript");
+
+		float newX = script.transform.position.x;
+		float newY = transform.position.y;
+		float newZ = transform.position.z;
+
+		transform.position = new Vector3 (newX, newY, newZ);
+	}
+}
