5a1f1c3190d62c7a2e0eabb6907c38524e16deb6
zygoth <zygoth2@gmail.com>
2015-02-12 14:51:58 -0700

    added spikes

---
+++ Vertigo/Assets/Scripts/SpikeScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		GameObject character = GameObject.Find ("Character");
+		CharacterControllerScript script = (CharacterControllerScript) character.GetComponent ("CharacterControllerScript");
+		script.hurt ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
