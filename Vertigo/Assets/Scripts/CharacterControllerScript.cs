830505a4c58762d888fcf71158f87ea05af9ca2b
zygoth <zygoth2@gmail.com>
2015-02-19 16:27:12 -0700

    changed character collider to polygon, not perfect but better than before.

--- Vertigo/Assets/Scripts/CharacterControllerScript.cs
+++ Vertigo/Assets/Scripts/CharacterControllerScript.cs
@@ -126,10 +126,12 @@ public class CharacterControllerScript: MonoBehaviour
 		{
 			if(horizontal == 0)
 			{
+				//rigidbody2D.AddForce (new Vector3(500f, 0, 0));
 				rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, vertical * MAXSPEED);
 			}
 			else
 			{
+				//rigidbody2D.AddForce (new Vector3(horizontal * 600f, 0, 0));
 				rigidbody2D.velocity = new Vector2(horizontal * MAXSPEED, rigidbody2D.velocity.y);
 			}
 		}
