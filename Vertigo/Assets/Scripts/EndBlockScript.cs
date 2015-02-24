877be161f1a53c58b732e686362115f653e8e1e5
zygoth <zygoth2@gmail.com>
2015-02-24 11:33:50 -0700

    added basic level ending block

---
+++ Vertigo/Assets/Scripts/EndBlockScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndBlockScript : MonoBehaviour 
+{
+	public string levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Application.LoadLevel (levelToLoad);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
