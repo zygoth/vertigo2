ae60da449d2daf275e9837565b6dd4898b1b5b02
zygoth <zygoth2@gmail.com>
2015-02-26 14:53:57 -0700

    started on breakable blocks.

---
+++ Vertigo/Assets/Scripts/BreakableBlock.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakableBlock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log ("collided with " + collision.collider.name);
+		if (collision.collider.name == "Shot") 
+		{
+			Destroy (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
