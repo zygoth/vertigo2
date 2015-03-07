460a64fa457ba7977e9b6030ace3bc86607b9d0c
zygoth <zygoth2@gmail.com>
2015-03-06 18:23:33 -0700

    exploding shots for no good reason, aww yeahhh.

---
+++ Vertigo/Assets/Scripts/Explosion.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void destroySelf()
+	{
+		Destroy (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Vertigo/Assets/Scripts/Shot.cs
+++ Vertigo/Assets/Scripts/Shot.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Shot : MonoBehaviour {
 
+	public GameObject explosionPrefab;
 	private Vector2 velocityVector;
 	// Use this for initialization
 	void Start () 
@@ -12,10 +13,13 @@ public class Shot : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		Debug.Log ("collided with a block!");
+		if(other.name == "Breakable Block")
+		{
+			Destroy (other.gameObject);
+		}
 		if(other.gameObject.layer == 0) // default layer, so it's a collidable block
 		{
-
+			Instantiate (explosionPrefab, transform.position, Quaternion.identity);
 			Destroy (gameObject);
 		}
 	}
