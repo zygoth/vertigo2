e08f41ab4cea98190e5bd4f2397b53b686b8c899
zygoth <zygoth2@gmail.com>
2015-03-16 11:51:09 -0600

    started on music system.

---
+++ Vertigo/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		GameObject introMusic = GameObject.Find ("Intro Music");
+		introMusic.audio.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
