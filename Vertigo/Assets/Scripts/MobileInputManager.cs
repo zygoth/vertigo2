5af7e74d2db53e57a91674c7a85fc59da240e0c4
zygoth <zygoth2@gmail.com>
2015-04-08 11:17:55 -0600

    Fixed touch input, and added an unnecessary utility.

--- Vertigo/Assets/Scripts/MobileInputManager.cs
+++ Vertigo/Assets/Scripts/MobileInputManager.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class MobileInputManager : MonoBehaviour {
 
+	private int SCREENWIDTH = 32;
+	private int SCREENHEIGHT = 24;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,6 +14,15 @@ public class MobileInputManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update()
 	{
+		/*
+		if(Input.GetMouseButtonDown (0))
+		{
+			Debug.LogError ("touch at screen position: " + Input.mousePosition.x + " " + Input.mousePosition.y);
+			Vector3 worldClickPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+			Debug.LogError ("touch at world position: " + worldClickPoint.x + " " + worldClickPoint.y);
+			return;
+		}*/
+
 		if (Input.touchCount == 0)
 		{
 			return;
@@ -20,29 +32,31 @@ public class MobileInputManager : MonoBehaviour {
 		float YAxis = 0f;
 		bool jump = false;
 		bool shoot = false;
+		GameObject camera = GameObject.Find ("Main Camera");
+		CameraController cameraScript = (CameraController) camera.GetComponent ("CameraController");
+		Vector2 bottomLeftCorner = new Vector2 (cameraScript.transform.position.x - (SCREENWIDTH / 2), cameraScript.transform.position.y - (SCREENHEIGHT / 2));
 
-		// Look for all fingers
+		// Look for all touches
 		for (int i = 0; i < Input.touchCount; i++)
 		{
 			Touch touch = Input.GetTouch(i);
-			
+			Vector3 worldPoint = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y,0));
 
-
-			if (touch.position.x < AspectUtility.xOffset + AspectUtility.screenWidth / 8)
+			if (worldPoint.x < bottomLeftCorner.x + SCREENWIDTH / 8)
 			{
 				XAxis = -1;
 			}
-			if (touch.position.x > AspectUtility.xOffset + AspectUtility.screenWidth / 8 && touch.position.x < AspectUtility.xOffset + AspectUtility.screenWidth / 4)
+			if (worldPoint.x > bottomLeftCorner.x + SCREENWIDTH / 8 && worldPoint.x < bottomLeftCorner.x + SCREENWIDTH / 4)
 			{
 				XAxis = 1;
 			}
-			if (touch.position.y > AspectUtility.yOffset && touch.position.y < AspectUtility.yOffset + AspectUtility.screenHeight / 2 &&
-			    touch.position.x < AspectUtility.yOffset + AspectUtility.screenWidth * 1 / 4)
+			if (worldPoint.y > bottomLeftCorner.y && worldPoint.y < bottomLeftCorner.y + SCREENHEIGHT / 2 &&
+			    worldPoint.x < bottomLeftCorner.y + SCREENWIDTH * 1 / 4)
 			{
 				YAxis = -1;
 			}
-			if (touch.position.y > AspectUtility.yOffset + AspectUtility.screenHeight / 2 && touch.position.y < AspectUtility.yOffset + AspectUtility.screenHeight &&
-			    touch.position.x < AspectUtility.yOffset + AspectUtility.screenWidth * 1 / 4)
+			if (worldPoint.y > bottomLeftCorner.y + SCREENHEIGHT / 2 && worldPoint.y < bottomLeftCorner.y + SCREENHEIGHT &&
+			    worldPoint.x < bottomLeftCorner.y + SCREENWIDTH * 1 / 4)
 			{
 				YAxis = 1;
 			}
@@ -54,13 +68,13 @@ public class MobileInputManager : MonoBehaviour {
 			}
 
 			// Jump check
-			if (touch.position.x > AspectUtility.xOffset + AspectUtility.screenWidth * 7 / 8)
+			if (worldPoint.x > bottomLeftCorner.x + SCREENWIDTH * 7 / 8)
 			{
 				jump = true;
 			}
 
 			// Shoot
-			if (touch.position.x < AspectUtility.xOffset + AspectUtility.screenWidth * 7 / 8 && touch.position.x > AspectUtility.xOffset + AspectUtility.screenWidth * 3 / 4)
+			if (worldPoint.x < bottomLeftCorner.x + SCREENWIDTH * 7 / 8 && worldPoint.x > bottomLeftCorner.x + SCREENWIDTH * 3 / 4)
 			{
 				shoot = true;
 			}
