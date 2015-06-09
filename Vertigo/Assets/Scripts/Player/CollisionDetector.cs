93da84c951511bcb3d519159c3a37df8b7b5fa21
zygoth <zygoth2@gmail.com>
2015-06-09 17:37:44 -0600

    yah

---
+++ Vertigo/Assets/Scripts/Player/CollisionDetector.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionDetector
+{
+	public CollisionDetectorRaycast[] raycasts;
+	public float EPSILON = .1f;
+	private LayerMask colliderMask;
+	public float rotation = 0f;
+
+	public CollisionDetector(LayerMask colliderMask, params CollisionDetectorRaycast[] r)
+	{
+		raycasts = r;
+		this.colliderMask = colliderMask;
+	}
+
+	/*
+	 * Gets the maximum distance this object can move in a specified
+	 * direction (*angle*).  Does this by taking any raycasts attached
+	 * to this detector with a similar angle and taking the min of their
+	 * results.  Returns -1 if there are no raycasts going that direction
+	 * or none of the raycasts hit anything.
+	 */
+	public float getMaxDistance(Vector2 objectPosition, float angle)
+	{
+		float maxDistance = -1f;
+		foreach (CollisionDetectorRaycast ray in raycasts)
+		{
+			if (numberDistance(ray.angle, angle) < EPSILON)
+			{ 
+				CollisionDetectorRaycast rotatedRaycast = ray.Rotate (0);
+
+				RaycastHit2D hit = Physics2D.Raycast (ray.relativePosition + objectPosition, new Vector2(Mathf.Cos (ray.angle), Mathf.Sin(ray.angle)), 
+				                                      distance: ray.length, layerMask: colliderMask);
+
+				if(hit.collider != null)
+				{
+					maxDistance = Mathf.Max (maxDistance, (hit.point - (ray.relativePosition + objectPosition)).magnitude);
+				}
+			}
+		}
+
+		return maxDistance;
+	}
+
+	private float numberDistance(float nr1, float nr2)
+	{
+		return Mathf.Abs(nr1 - nr2);
+	}
+}
---
+++ Vertigo/Assets/Scripts/Player/CollisionDetectorRaycast.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * This class represents a raycast used by a CollisionDetector object.
+ * Its purpose is to communicate to that class the kind of raycast it should
+ * use to determine collisions.
+ */
+public class CollisionDetectorRaycast
+{
+	public float angle { get; set; }
+	public Vector2 relativePosition { get; set; }
+	public float length { get; set; }
+
+	public CollisionDetectorRaycast Rotate(float radians)
+	{
+		return new CollisionDetectorRaycast{angle = (this.angle + radians) % (Mathf.PI * 2f),
+											relativePosition = this.relativePosition.Rotate (radians),
+											length = this.length};
+	}
+
+	public void testRotate()
+	{
+		Debug.Log ("testing raycast rotate");
+		CollisionDetectorRaycast testRaycast = new CollisionDetectorRaycast {angle = 0f, relativePosition = new Vector2(1f, -1f), length = 1f};
+		CollisionDetectorRaycast rotatedRay = testRaycast.Rotate (Mathf.PI / 2f);
+		Debug.Log ("rotated raycast angle: " + rotatedRay.angle);
+		Debug.Log ("rotated raycast position: " + rotatedRay.relativePosition);
+		Debug.Log ("rotated raycast length: " + rotatedRay.length);
+	}
+}
+
+public static class Vector2Extension
+{
+	public static Vector2 Rotate(this Vector2 v, float radians)
+	{
+		return Quaternion.Euler(0, 0, Mathf.Rad2Deg * radians) * v;
+	}
+}
